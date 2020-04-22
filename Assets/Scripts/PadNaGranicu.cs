bf7219565e19ed44af391bf5291f6ab84511b976
Kasapović Fabijan <Korisnik@DESKTOP-8SVIDQP>
2020-04-22 08:09:13 +0200

    Initial commit

---
+++ Assets/Scripts/PadNaGranicu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadNaGranicu : MonoBehaviour {
+
+     void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Die();
+        }
+          
+        
+    }
+    void Die()
+        {
+            LevelMenager man = GameObject.Find("LevelMenager").GetComponent<LevelMenager>();
+            man.LoadLevel("Win");
+            Destroy(gameObject);
+        }
+}
